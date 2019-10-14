d0b4d4d81affddae70fa9ce31ac8e13ca71928db
Bahruz <bahruz.code@gmail.com>
2019-10-14 17:54:25 +0400

    Capitalized

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,38 @@
-import React from 'react';
-import Posts from './Posts/Posts';
-import './App.css';
-
+import React from "react";
+import Posts from "./Posts/Posts";
+import "./App.css";
 
 class App extends React.Component {
-    state = {
-        posts: [
+  state = {
+    posts: [],
+    isLoading: true
+  };
 
-        ],
-        isLoading: true
-    }
+  async fetchPosts() {
+    await fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          posts: data,
+          isLoading: false
+        })
+      );
+  }
 
-    async fetchPosts() {
-        await fetch('https://jsonplaceholder.typicode.com/posts')
-            .then(response => response.json())
-            .then(data => this.setState({
-                posts: data,
-                isLoading: false
-            }));
-    }
+  componentDidMount() {
+    this.fetchPosts();
+  }
 
-    componentDidMount(){
-         this.fetchPosts();   
-    }
-
-    render() {
-        return (
-            <div className="b-container">
-                <Posts loading={this.state.isLoading} list={this.state.posts}/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="b-container">
+        <div className="b-heading">
+          <h3>Posts</h3>
+        </div>
+        <Posts loading={this.state.isLoading} list={this.state.posts} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const Post = props => {
-    return (
-        <div className="b-post">
-            <p className="b-post-title">{props.post.title}</p>
-            <p className="b-post-body">{props.post.body.slice(0,150)}</p>
-        </div>
-    )
-}
+  return (
+    <div className="b-post">
+      <p className="b-post-title">
+        {props.post.title.charAt(0).toUpperCase() +
+          props.post.title.substring(1)}
+      </p>
+      <p className="b-post-body">{props.post.body.slice(0, 150)}</p>
+    </div>
+  );
+};
 
 export default Post;
